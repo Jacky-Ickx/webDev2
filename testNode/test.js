563ab3780035ba80048d484a814e5eccfd0c2aa1
Jannik Springer <jannik.springer.30.40@gmail.com>
2021-09-24 11:49:50 +0200

    First Node Test
    adding node_modules to gitignore

---
+++ testNode/test.js
@@ -0,0 +1,6 @@
+var colors = require('colors');
+
+console.log("Hello World!".green);
+console.log("Node.js has been successfully installed.".blue);
+console.log("nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan".rainbow);
+console.log("Murica, FUCK YEAH! Coming again to save the motherfucking day, yeah!".america);
